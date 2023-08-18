689bf992615d8193dabe0fcfd6a7ed616c055cd2
greyyT <nhannguyen8338@gmail.com>
2023-08-18 21:33:23 +0700

    Try to fix routing issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
     },
-  },
+  };
+
+  if (command !== 'serve') {
+    config.base = '/lunar-eprocurement-system/';
+  }
+
+  return config;
 });
